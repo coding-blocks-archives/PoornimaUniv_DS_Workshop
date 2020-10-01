5135dc3c8b279cb4aa8e46632ecab1f1b5ef660e
nikhilmishra03 <56030038+nikhilmishra03@users.noreply.github.com>
2020-10-01 10:20:58 +0530

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,13 +1,9 @@
-#include <iostream>
+#include <bits/stdc++.h>  
 using namespace std;
 int main(){
-    // print "Coding Blocks" and enter
-    // cout << "Coding Blocks" << endl;
-    // cout << "Coding Blocks" << endl;
-    // cout << "Coding Blocks" << endl;
-    // cout << "Coding Blocks" << endl;
-    // cout << "Coding Blocks" << endl;
-    for(int i = 0; i < 5; ++i){
-        cout << "Coding Blocks" << endl;
+int i=0;
+while (i<5){      //using while loops may do the trick easily as a simple condition applied
+    cout<<"coding blocks"<<endl;
+    i++;
     }
 }
